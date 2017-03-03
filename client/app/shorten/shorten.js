c88686c5534898af1aeaeb7c179e9ecd42233682
elijah-schow <elijah.schow@gmail.com>
2017-03-03 15:52:51 -0600

    implement shorten

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,16 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+.controller('ShortenController', function ($http, $scope, $location, Links) {
+  $scope.link = {};
+
+  $scope.addLink = function() {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: $scope.link
+    })
+    .then(function (resp) {
+      return resp;
+    });
+  };
 });
