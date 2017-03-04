ac69bdaf5ab9b04ceb8f57c947a63030e84c0510
elijah-schow <elijah.schow@gmail.com>
2017-03-03 20:01:19 -0600

    finished most of the things

--- client/app/app.js
+++ client/app/app.js
@@ -17,11 +17,13 @@ angular.module('shortly', [
     })
     .when('/links', {
       templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+      controller: 'LinksController',
+      authenticate: true
     })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
-      controller: 'ShortenController'
+      controller: 'ShortenController',
+      authenticate: true
     })
     .otherwise({
       templateUrl: 'app/links/links.html',
@@ -58,6 +60,7 @@ angular.module('shortly', [
   // and send that token to the server to see if it is a real user or hasn't expired
   // if it's not valid, we then redirect back to signin/signup
   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    console.log("-------->this func is ran")
     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
       $location.path('/signin');
     }
